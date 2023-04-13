99cc8e8713a483eb9d0e985774948573c01de204
anilu556 <anilu556@gmail.com>
2023-04-13 13:05:40 -0600

    title modification

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -7,7 +7,7 @@ class Hello extends React.Component {
            
         <React.Fragment>  
            <h1 className="home_port">Ana González</h1> 
-           <p className="home_title">Jr. Full-Stack Web Developer</p>
+           <p className="home_title">Front-end Web Developer</p>
            <p className="home_about"> I have a Bachelor's Degree in Communication and ever since I graduated, I have been immersed in the digital world.</p>
            <p className="home_about">I worked as web editor in publishing houses, which awakened in me the curiosity to learn how the systems behind the websites we interact with daily really work. 
            Today, after graduating from a bootcamp, I write code as a web developer.</p>
